e8889bd54aeabd5df69dea00e521da06cf05f1f2
Sonu-Pandey1 <sonupandeybaldeo2003@gmail.com>
2024-12-10 12:22:07 +0530

    ok check-3

--- src/Components/Pin.jsx
+++ src/Components/Pin.jsx
@@ -2,19 +2,46 @@
 import { Link } from "react-router-dom";
 import "./Pin.scss";
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Fix Leaflet marker icon paths for production
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import markerIconRetina from "leaflet/dist/images/marker-icon-2x.png";
+
+// Override default icon settings
+const customIcon = new L.Icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIconRetina,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41], // Default icon size
+  iconAnchor: [12, 41], // Anchor point for the icon
+  popupAnchor: [1, -34], // Anchor point for the popup
+  shadowSize: [41, 41], // Shadow size
+});
 
 export default function Pin({ item }) {
   return (
     <div>
-      <Marker position={[item.lat, item.long]}>
+      <Marker
+        position={[item.lat, item.long]}
+        icon={customIcon} // Use the custom icon
+      >
         <Popup>
           <div className="popupContainer">
             <img src={item.image} alt="img" />
             <div className="textContainer">
-              <Link to={`${item.id}`} className=" text-truncate text-decoration-none" title={item.title}>
-              {item.title.length > 15 ? `${item.title.slice(0, 15)}...` : item.title}
+              <Link
+                to={`${item.id}`}
+                className="text-truncate text-decoration-none"
+                title={item.title}
+              >
+                {item.title.length > 15
+                  ? `${item.title.slice(0, 15)}...`
+                  : item.title}
               </Link>
-              <span className="bed">{item.badrooms} Badroom</span>
+              <span className="bed">{item.badrooms} Bedroom</span>
               <b>$ {item.price}</b>
             </div>
           </div>
